2e41b7b69a36db85e9908d4fab1fb227c3dd9226
cantonapo <wapangho@gmail.com>
2026-08-28 16:54:28 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'mpa-schedule-v1';
+const CACHE_NAME = 'mpa-schedule-v2';
 
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icon-192-v2.png',
+  './icon-512-v2.png'
 ];
 
 self.addEventListener('install', event => {
